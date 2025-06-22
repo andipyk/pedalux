4bf2dc674c9209a746f4c2027fb016c38a9f407d
andipyk <andisyafrianda@gmail.com>
2025-06-22 21:21:03 +0700

    refactor(resources): remove unused imports from filament resources

--- app/Filament/Resources/TestimonialResource.php
+++ app/Filament/Resources/TestimonialResource.php
@@ -3,15 +3,12 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\TestimonialResource\Pages;
-use App\Filament\Resources\TestimonialResource\RelationManagers;
 use App\Models\Testimonial;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class TestimonialResource extends Resource
 {
